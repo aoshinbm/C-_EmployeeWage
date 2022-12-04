0d11d719b4bb81f78a7a812f19e4dcaf3508af65
aoshinbm <aoshinbm28797@gmail.com>
2022-12-05 04:06:48 +0530

    Added: Employee part-time UC3 added

--- EmpWage.cpp
+++ EmpWage.cpp
@@ -7,21 +7,32 @@ using namespace std;
 int wageperhr=20;
 int wage;
 int fullday=8;
+int parttime=5;
 
 void fulltimejob()
 {
     wage=wageperhr*fullday;
     cout<<"\nFULL TIME Wage :" <<wage;
 }
+void parttimejob()
+{
+    wage=wageperhr*parttime;
+    cout<<"\n PART TIME Wage :" <<wage;
+}
 void attendance(){
     srand(time(0));
-    int attendance =(rand()%2);
+    int attendance =(rand()%3);
     cout<< attendance;
     if (attendance == 1)
     {
         cout<<"\nEmployee is present ";
         fulltimejob();
     } 
+    else if (attendance == 2)
+    {
+        cout<<"\nEmployee is present";
+        parttimejob();
+    }    
     else
     {
         cout<<"\nEmployee is absent";
